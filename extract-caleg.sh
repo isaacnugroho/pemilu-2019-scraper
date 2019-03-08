99f42800b6de6bfa1d9016821a18eefa81532cf0
isaac <isaac.nugroho@gmail.com>
2019-03-09 01:49:19 +0700

    bug updated!

--- extract-caleg.sh
+++ extract-caleg.sh
@@ -2,46 +2,46 @@
 
 
 i=0
-for p in result/caleg/dapil_dpr_caleg_*.json; do
+for p in result/caleg/caleg_dpr_*.json; do
   v=$(echo $p | grep -oE '[0-9]+')
   if [[ i -eq 0 ]]; then
-    jq -r --argjson dapilId $v '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
-        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @tsv' $p > result/dapil_dpr_caleg.csv
+    jq -r --argjson dapilId "$v" '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
+        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @tsv' $p > result/caleg_dpr.csv
 
     i=1
   else
-    jq -r --argjson dapilId $v '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
-        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @tsv' $p >> result/dapil_dpr_caleg.csv
+    jq -r --argjson dapilId "$v" '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
+        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @tsv' $p >> result/caleg_dpr.csv
 
   fi
 done
 
 i=0
-for p in result/caleg/dapil_provinsi_caleg_*.json; do
+for p in result/caleg/caleg_provinsi_*.json; do
   v=$(echo $p | grep -oE '[0-9]+')
   if [[ i -eq 0 ]]; then
-    jq -r --argjson dapilId $v '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
-        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @tsv' $p > result/dapil_provinsi_caleg.csv
+    jq -r --argjson dapilId "$v" '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
+        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @tsv' $p > result/caleg_provinsi.csv
 
     i=1
   else
-    jq -r --argjson dapilId $v '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
-        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @tsv' $p >> result/dapil_provinsi_caleg.csv
+    jq -r --argjson dapilId "$v" '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
+        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @tsv' $p >> result/caleg_provinsi.csv
 
   fi
 done
 
 i=0
-for p in result/caleg/dapil_kabupaten_caleg_*.json; do
+for p in result/caleg/caleg_kabupaten_*.json; do
   v=$(echo $p | grep -oE '[0-9]+')
   if [[ i -eq 0 ]]; then
-    jq -r --argjson dapilId $v '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
-        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @tsv' $p > result/dapil_kabupaten_caleg.csv
+    jq -r --argjson dapilId "$v" '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
+        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @tsv' $p > result/caleg_kabupaten.csv
 
     i=1
   else
-    jq -r --argjson dapilId $v '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
-        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @tsv' $p >> result/dapil_kabupaten_caleg.csv
+    jq -r --argjson dapilId "$v" '[.[] | { idDapil: $dapilId, namaPartai, id, noUrut, nama, stringJenisKelamin, originalFilename } ] |
+        (map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $rows[] | @tsv' $p >> result/caleg_kabupaten.csv
 
   fi
 done
