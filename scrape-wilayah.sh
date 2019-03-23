bcebe219e0c2fcf30d146de4bac999ecafa4e596
isaac <isaac.nugroho@gmail.com>
2019-03-24 01:34:02 +0700

    wip

--- scrape-wilayah.sh
+++ scrape-wilayah.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
 
+getJson() {
+  if curl -k "$1" -o /tmp/temp_$$; then
+    if [[ -f /tmp/temp_$$ && $(jq -r '.[]' /tmp/temp_$$ | wc -l) -gt 0 ]]; then
+      cat /tmp/temp_$$ >> $2
+    else
+      echo $1 >> scrape-wilayah-error.txt
+    fi
+  fi
+}
 
-curl -k https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/0?_=$(date +%s) > result/wilayah/provinsi.json
+getJson https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/0?_=$(date +%s) result/wilayah/provinsi.json
 
 prov=$(jq '.[] | .id' result/wilayah/provinsi.json)
 for p in $prov; do
-  curl -k https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/$p?_=$(date +%s) > result/wilayah/provinsi_$p.json
+  rm -f result/wilayah/provinsi_$p.json
+  getJson https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/$p?_=$(date +%s) result/wilayah/provinsi_$p.json
 done
 for q in result/wilayah/provinsi_*.json; do
   kab=$(jq '.[] | .id' $q)
   for r in $kab; do
-    curl -k https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/$r?_=$(date +%s) > result/wilayah/kabupaten_$r.json
+    rm -f result/wilayah/kabupaten_$r.json
+    getJson https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/$r?_=$(date +%s) result/wilayah/kabupaten_$r.json
   done
 done
 for q in result/wilayah/kabupaten_*.json; do
   kec=$(jq '.[] | .id' $q)
   for r in $kec; do
-    curl -k https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/$r?_=$(date +%s) > result/wilayah/kecamatan_$r.json
+    rm -f result/wilayah/kecamatan_$r.json
+    getJson  https://infopemilu.kpu.go.id/pileg2019/api/wilayah/cari/$r?_=$(date +%s) result/wilayah/kecamatan_$r.json
   done
 done
