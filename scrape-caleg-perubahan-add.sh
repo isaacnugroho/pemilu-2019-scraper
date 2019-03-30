3198166cf019d185278a2ada96cf3804e488824a
isaac <isaac.nugroho@gmail.com>
2019-03-30 15:27:23 +0700

    added perubahan delta

---
+++ scrape-caleg-perubahan-add.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [[ ! -e result/partai.json ]]; then
+  curl -k https://infopemilu.kpu.go.id/pileg2019/pencalonan/allparpol.json?_=$(date +%s) > result/partai.json
+fi
+partai=$(jq '.[].nomorUrut' result/partai.json)
+
+getJson() {
+  if curl -k "$1" -o /tmp/temp_$$; then
+    if [[ -f /tmp/temp_$$ && $(jq -r '.[]' /tmp/temp_$$ | wc -l) -gt 0 ]]; then
+      cat /tmp/temp_$$ >> $2
+    else
+      echo $1 $2 >> scrape-caleg-perubahan-error.txt
+    fi
+  fi
+}
+
+dapil=$(jq '.[].dapil[].id' result/dapil/dapil_dpr_wilayah_*.json)
+for d in $dapil; do
+  for p in $partai; do
+    if [[ ! -f result/caleg/caleg_dpr_perubahan_$d_$p.json ]]; then
+      getJson https://infopemilu.kpu.go.id/pileg2019/pencalonan/pengajuan-calon/$d/$p/perubahan-dct?_=$(date +%s) result/caleg/caleg_dpr_perubahan_$d_$p.json
+    fi
+  done
+done
+
+dapil=$(jq '.[].dapil[].id' result/dapil/dapil_provinsi_wilayah_*.json)
+for d in $dapil; do
+  for p in $partai; do
+    if [[ ! -f result/caleg/caleg_dpr_perubahan_$d_$p.json ]]; then
+      getJson https://infopemilu.kpu.go.id/pileg2019/pencalonan/pengajuan-calon/$d/$p/perubahan-dct?_=$(date +%s) result/caleg/caleg_provinsi_perubahan_$d_$p.json
+    fi
+  done
+done
+
+dapil=$(jq '.[].dapil[].id' result/dapil/dapil_kabupaten_wilayah_*.json)
+for d in $dapil; do
+  for p in $partai; do
+    if [[ ! -f result/caleg/caleg_dpr_perubahan_$d_$p.json ]]; then
+      getJson https://infopemilu.kpu.go.id/pileg2019/pencalonan/pengajuan-calon/$d/$p/perubahan-dct?_=$(date +%s) result/caleg/caleg_kabupaten_perubahan_$d_$p.json
+    fi
+  done
+done
