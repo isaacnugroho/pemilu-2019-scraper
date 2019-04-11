e9c8658c2848fb287f923fbb2ef528e670ef0024
isaac <isaac.nugroho@gmail.com>
2019-04-11 11:19:13 +0700

    wip

--- scrape-redo.sh
+++ scrape-redo.sh
@@ -3,7 +3,7 @@
 getJson() {
   if curl -k "$1" -o /tmp/temp_$$; then
     # do not check empty && $(jq -r '.[]' /tmp/temp_$$ | wc -l) -gt 0 
-    if [[ -f /tmp/temp_$$ ]]; then
+    if [[ -f /tmp/temp_$$ && $(jq -r '.' /tmp/temp_$$ 2>/dev/null) ]]; then
       cat /tmp/temp_$$ >> $2
     else
       echo $1 $2 >> scrape-redo-error.txt
